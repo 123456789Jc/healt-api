4811ec3d5214efc2cab357b93983e2f3f1bca09b
Johan Cortes <cortesroquejohansebastian@gmail.com>
2025-04-29 19:21:24 -0500

    actualizacion auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config'; // Importa ConfigModule
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Agrega esta línea para cargar las variables de entorno
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
