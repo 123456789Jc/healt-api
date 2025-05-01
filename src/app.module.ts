ab82a7207c0cbe75f0fc2bcd471b26c408b9a865
Johan Cortes <cortesroquejohansebastian@gmail.com>
2025-04-30 19:06:12 -0500

    Actualizacion api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config'; // Importa ConfigModule
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // Agrega esta línea para cargar las variables de entorno
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que ConfigModule esté disponible en todos los módulos sin necesidad de importarlo nuevamente
+    }),
     AuthModule
   ],
   controllers: [AppController],
